04d1eb11e716f0b6a4ea8ddf7efb80c34d77aba0
Vitalii Lymar <vlm191997@gmail.com>
2020-05-04 16:01:41 +0300

    Create module Builder

---
+++ Weatherer/ModuleBuilder .swift
@@ -0,0 +1,25 @@
+//
+//  ModuleBuilder .swift
+//  Weatherer
+//
+//  Created by HexaHack on 04.05.2020.
+//  Copyright © 2020 HexaHack. All rights reserved.
+//
+
+import UIKit
+
+protocol Builder {
+    static func createMainModule() -> UIViewController
+}
+
+class ModuleBuilder: Builder {
+    static func createMainModule() -> UIViewController {
+        let view = MainViewController()
+        let locationService = LocationService()
+        let networkService = NetworkService(locationService: locationService)
+        let presenter = MainPresenter(view: view, networkService: networkService)
+        view.presenter = presenter
+    }
+    
+    
+}
